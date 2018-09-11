bbff9e9331146056feeac20135eb1dccfc9390eb
Andrei Chaplygin <gotacoin@gmail.com>
2018-09-11 03:14:58 -0700

    Added data cleaning logic and created separate notebook for each analysis

--- Code/config.py
+++ Code/config.py
@@ -1,1 +1,3 @@
-kaggle_data_path = '../Data/Auto/gun-violence-data_01-2013_03-2018.csv'
+incidents_raw_data_path = '../Data/Auto/gun-violence-data_01-2013_03-2018.csv'
+incidents_clean_data_path = '../Data/Auto/gun-violence-data_01-2013_03-2018_clean.csv'
+states_data_path = '../Data/Static/states.csv'
---
+++ Code/shared.py
@@ -0,0 +1,13 @@
+def split_value(value: str) -> dict:
+    '''
+    Converts string value that looks like 'key_0::value_0||key_1:value_1'
+    into a dictionary { 'key_0': 'value_0', 'key_1': 'value1' }
+    '''
+    result = {}
+    if not(value):
+        return result
+    items = value.split('||')
+    for item in items:
+        key_value = item.split('::')
+        if len(key_value) > 1:
+            result[key_value]
