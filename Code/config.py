234db8dc3a3601fee96416816ef804966afbbb2f
Craig Taylor <craigtaylor86@hotmail.com>
2018-09-13 21:37:21 -0700

    Gun Laws Data Cleaning and Scatter plot

--- Code/config.py
+++ Code/config.py
@@ -1,3 +1,4 @@
 incidents_raw_data_path = '../Data/Auto/gun-violence-data_01-2013_03-2018.csv'
 incidents_clean_data_path = '../Data/Auto/gun-violence-data_01-2013_03-2018_clean.csv'
 states_data_path = '../Data/Static/states.csv'
+dict_path = '../Data/Static/State_Star_Dict.csv'
